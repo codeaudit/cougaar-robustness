1f45f3861387d12c48722e81c5ed4b0eb32e4a9d
sford <sford>
2003-04-11 02:07:56 +0000

    added msglog society transformation rule

---
+++ msglog/test/transform-SA-1AD.bat
@@ -0,0 +1,3 @@
+ruby %COUGAAR_INSTALL_PATH%\csmart\config\bin\transform_society.rb -i %COUGAAR_INSTALL_PATH%\csmart\config\societies\SC-1AD-STRIPPED.xml -r %COUGAAR_INSTALL_PATH%\csmart\config\rules\robustness\msglog.rule -o %COUGAAR_INSTALL_PATH%\csmart\config\societies\SC-IAD-STRIPPED-MSGLOG.xml
+
+pause
