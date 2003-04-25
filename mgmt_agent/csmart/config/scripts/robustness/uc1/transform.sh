2ac0bfa7d6ddadf297de0ca2ee2ab93e4ff9aea7
rsnyder <rsnyder>
2003-04-25 14:03:06 +0000

    Updated to reflect revised directory structure

--- mgmt_agent/csmart/config/scripts/robustness/uc1/transform.sh
+++ mgmt_agent/csmart/config/scripts/robustness/uc1/transform.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-ACMEDIR='/usr/local/acme'
+#ACMEDIR='/usr/local/acme'
+ACMEDIR="$COUGAAR_INSTALL_PATH/csmart"
 WRKDIR=`pwd`
+RULESDIR="$WRKDIR/../../../rules/robustness/uc1"
 
-java -cp ../lib/Robustness_mic_mgmt_agent.jar org.cougaar.tools.robustness.ma.util.ACMEXmlTransformer $1 $2 tmp.xml ../aruc1.xsl
+java -cp $COUGAAR_INSTALL_PATH/lib/ar_mic.jar org.cougaar.tools.robustness.ma.util.ACMEXmlTransformer $1 $2 tmp.xml aruc1.xsl
 
 cd $ACMEDIR/acme_scripting/bin
-ruby transform_society.rb -i $WRKDIR/tmp.xml -r $WRKDIR/transform_rules
+ruby transform_society.rb -i $WRKDIR/tmp.xml -r $RULESDIR
 mv new-tmp.xml $WRKDIR/tmp1.xml
 cd $WRKDIR
 cat tmp1.xml | sed "s/localhost/$3/" > aruc1_society.xml
