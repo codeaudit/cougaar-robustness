f2062ba7e2c0295f3df6ea32c879e32008e90a5a
rsnyder <rsnyder>
2003-06-19 12:50:36 +0000

    Updates to LoadBalancer and CommunityReady actions/states.  Also removed unused code.

--- mgmt_agent/csmart/config/lib/robustness/uc1/aruc1_actions_and_states.rb
+++ mgmt_agent/csmart/config/lib/robustness/uc1/aruc1_actions_and_states.rb
@@ -1,365 +1,31 @@
 module Cougaar
-  
-  ORIGAGENTS_CONST = Hash.new(0) #records all nodes and child agents number before node restarting
-  CURRENTAGENTS_CONST = Hash.new(0) #records all nodes and child agents number after node restarting
-  PORTS_CONST = Hash.new(0) #record each node and it's port address
-  ALLNODES_CONST = [] #record names of all current running nodes
-  KILLNODE_CONST = "" #which one is being killed right now?
-  ShiftFlag_CONST = "false" #do we need remove the first node in all node list?
-  Times_CONST = [] # record time before and after restarting a node  
-  KillHost_CONST = "" #the host of the most currently killed node
-  LoadBalancer_CONST = "false"
-
-  class CommunityController
-    def initialize(run, communityName, agentsCount)
-      @run = run
-      @community = communityName
-      @agentsCount = agentsCount
-    end
-    #Get all addresses of active nodes, the address will be in format host:port. this method
-    #is only run once after the society is started.
-    def getPorts
-      @run.society.each_node do |node|
-       temp = node.name
-       ALLNODES_CONST[ALLNODES_CONST.length] = temp if not ALLNODES_CONST.include?(temp)
-       hostName = node.host.name
-       if hostName[0..4] == "host|"
-          hostName[0..4] = ""
-       end
-       address = nil
-       port = 8800
-       while address == nil
-         result,uri = Cougaar::Communications::HTTP.get("http://#{hostName}:#{port}/agents?format=text")
-         begin
-           result.each_line do |line| 
-	     if line.strip == temp
-               address = "#{hostName}:#{port}"
-               break
-             end
-           end
-         rescue NameError
-           next
-         end
-         port += 1
-       end
-       PORTS_CONST[temp] = address
-     end
-     if PORTS_CONST.length != ALLNODES_CONST.length
-       sleep 2.seconds
-       getPorts 
-     end
-     ALLNODES_CONST.delete("RobustnessManagerNode") if ALLNODES_CONST.include?("RobustnessManagerNode")
-    end
-
-    #This method checked communityViewer servlet of every agent to test if the agents in given
-    #community equals the given number. Yes means the community is ready.
-    def isCommunityReady
-      @currentAgents = 0
-      if KILLNODE_CONST.length != 0
-        if LoadBalancer_CONST == "false"
-          if PORTS_CONST.has_key?(KILLNODE_CONST)
-            PORTS_CONST.delete(KILLNODE_CONST) 
-            puts "delete #{KILLNODE_CONST} from PORTS_CONST"
-          end
-	end
-      end
-      if LoadBalancer_CONST == "true"
-        LoadBalancer_CONST[0..LoadBalancer_CONST.length] = "false"
-      end
-      PORTS_CONST.each do |key, value|
-        if !(value.include?":") #this is a new node, try to get it's port address
-          puts "meet new node: #{key}"
-          sleep 1.minutes
-          address = nil
-          port = "8800"
-          while address == nil
-            temp = "#{value}:#{port}"
-            if PORTS_CONST.has_value?(temp)
-              tport = port.to_i + 1
-              port = tport.to_s
-              address = nil
-	    else
-              begin 
-                result,uri = Cougaar::Communications::HTTP.get("http://#{temp}/agents?format=text")
-                address = temp
-                PORTS_CONST[key] = address
-                value = address
-                break
-              rescue
-                puts "try to get port of #{key} failed"
-                sleep 5.seconds
-              end
-            end
-          end
-        end # end if !(value.include?":")
-	    begin
-              result2,uri2 = Cougaar::Communications::HTTP.get("http://#{value}/$#{key}/communityViewer")
-	      result2.each_line do |line2|
-		html = line2.strip
-                if html.include?"community="
-		  index = html.index('community=')
-                  html[0, index+10] = ""
-                  index = html.index('>')
-                  html[index, html.length] = ""
-                  if html == @community
-		    #@currentAgents += 1
-		    result3,uri3 = Cougaar::Communications::HTTP.get("http://#{value}/$#{key}/communityViewer?community=#{@community}")
-		    result3.each_line do |line3|
-		     agent = line3.strip
-		      if agent.include?"</a>"
-		        index = agent.index('>')
-			agent[0, index+1] = ""
-			index = agent.index('</a>')
-			agent[index, agent.length] = ""
-			if !(agent.include?"Community ")
-			  if ALLNODES_CONST.include?(agent)
-			    next
-			  end
-			  @currentAgents += 1
-			  #puts "#{agent} is in community: #{@currentAgents}"
-			end
-		      end
-		    end
-                    if @currentAgents >= @agentsCount
-                      if ShiftFlag_CONST == "true"
-  			one = ALLNODES_CONST.shift
-                        if one == "NAME_SERVER"
-                           ALLNODES_CONST.shift
-                        end
-                        ShiftFlag_CONST[0..ShiftFlag_CONST.length] = "false"
-		      end
-                      #KILLNODE_CONST[0..KILLNODE_CONST.length] = ALLNODES_CONST[0]
-		      return true
-		    end
-		    if @currentAgents < @agentsCount
-		      return false
-                    end
-		  end
-		end
-	      end
-        rescue NameError
-          next
-        end
-      end
-      return false
-    end
-  end
-  
 
   module States
-    class REAR_Community_Ready < Cougaar::State
-      DEFAULT_TIMEOUT = 30.minutes
-      PRIOR_STATES = ["SocietyRunning"]
-      def initialize(run, timeout=nil, &block)
-        super(run, timeout, &block)
-      end
-      def process
-        controller = ::Cougaar::CommunityController.new(run, "1AD-REAR-COMM", 18)
-        if PORTS_CONST.length == 0
-           sleep 3.minutes
-           controller.getPorts
-        end
-        #result = controller.isCommunityReady
-        while !(controller.isCommunityReady)
-          sleep 5.seconds
-        end
-        #str = ALLNODES_CONST.shift
-      end
-      def unhandled_timeout
-        @run.do_action "StopSociety" 
-        @run.do_action "StopCommunications"
-      end
-    end
 
-    class FWD_Community_Ready < Cougaar::State
+    class CommunitiesReady < Cougaar::State
       DEFAULT_TIMEOUT = 30.minutes
       PRIOR_STATES = ["SocietyRunning"]
-      def initialize(run, timeout=nil, &block)
+      def initialize(run, community_names, timeout=nil, &block)
         super(run, timeout, &block)
+        @community_names = community_names
       end
       def process
-        controller = ::Cougaar::CommunityController.new(run, "1AD-FWD-COMM", 14)
-        if PORTS_CONST.length == 0
-           sleep 2.minutes
-           controller.getPorts
+        loop = true
+        while loop
+          event = @run.get_next_event
+          @community_names.delete_if { |name| event.data=="Community #{name} Ready" }
+          if @community_names.size==0
+            loop = false
+          end
         end
-        #result = controller.isCommunityReady
-        while !(controller.isCommunityReady)
-          sleep 5.seconds
-        end
-        #str = ALLNODES_CONST.shift
       end
       def unhandled_timeout
-        @run.do_action "StopSociety" 
+        @run.do_action "StopSociety"
         @run.do_action "StopCommunications"
       end
     end
-
-    class CONUS_Community_Ready < Cougaar::State
-      DEFAULT_TIMEOUT = 30.minutes
-      PRIOR_STATES = ["SocietyRunning"]
-      def initialize(run, timeout=nil, &block)
-        super(run, timeout, &block)
-      end
-      def process
-        controller = ::Cougaar::CommunityController.new(run, "1AD-CONUS-COMM", 3)
-        if PORTS_CONST.length == 0
-           sleep 2.minutes
-           controller.getPorts
-        end
-        #result = controller.isCommunityReady
-        while !(controller.isCommunityReady)
-          sleep 5.seconds
-        end
-        #str = ALLNODES_CONST.shift
-      end
-      def unhandled_timeout
-        @run.do_action "StopSociety" 
-        @run.do_action "StopCommunications"
-      end
-    end
-
-    class TRANS_Community_Ready < Cougaar::State
-      DEFAULT_TIMEOUT = 30.minutes
-      PRIOR_STATES = ["SocietyRunning"]
-      def initialize(run, timeout=nil, &block)
-        super(run, timeout, &block)
-      end
-      def process
-        controller = ::Cougaar::CommunityController.new(run, "1AD-TRANS-COMM", 7)
-        if PORTS_CONST.length == 0
-           sleep 2.minutes
-           controller.getPorts
-        end
-        #result = controller.isCommunityReady
-        while !(controller.isCommunityReady)
-          sleep 5.seconds
-        end
-        #str = ALLNODES_CONST.shift
-      end
-      def unhandled_timeout
-        @run.do_action "StopSociety" 
-        @run.do_action "StopCommunications"
-      end
-    end
-
-    class Robustness_Community_Ready < Cougaar::State
-      DEFAULT_TIMEOUT = 30.minutes
-      PRIOR_STATES = ["SocietyRunning"]
-      def initialize(run, timeout=nil, &block)
-        super(run, timeout, &block)
-      end
-      def process
-        controller = ::Cougaar::CommunityController.new(run, "TINY-1AD-ROBUSTNESS-COMM", 42)
-        if PORTS_CONST.length == 0
-           sleep 2.minutes
-           controller.getPorts
-        end
-        #result = controller.isCommunityReady
-        if LoadBalancer_CONST == "true"
-	  sleep 4.minutes
-	end
-        while !(controller.isCommunityReady)
-          sleep 5.seconds
-        end
-        #str = ALLNODES_CONST.shift
-      end
-      def unhandled_timeout
-        @run.do_action "StopSociety" 
-        @run.do_action "StopCommunications"
-      end
-    end
-  end
 
   module Actions
-    class ShowResult < Cougaar::Action
-      PRIOR_STATES = ["SocietyRunning"]
-      def initialize(run, *nodes)
-        super(run)
-        @run = run
-        @nodes = nodes
-        $end = Time.new
-        #print "end time: ", $end.to_i, "\n"
-      end
-      def perform 
-        Times_CONST[1] = Time.new 
-        ShiftFlag_CONST[0..ShiftFlag_CONST.length] = "true"
-        puts "------------------------------------------"
-        if Times_CONST[0] != nil && Times_CONST[1] != nil
-          print "  Restart total time: ", Times_CONST[1].to_i - Times_CONST[0].to_i, " seconds\n"
-        end
-        countOrigNodes if ORIGAGENTS_CONST.length == 0  
-        countCurrentNodes
-        @origtotal = 0
-        ORIGAGENTS_CONST.each_value {|value| @origtotal += value}
-#ORIGAGENTS_CONST.each {|key, value| print key, " is ", value, "\n"}
-#puts "original total is: #{@origtotal}"
-        @currenttotal = 0
-        CURRENTAGENTS_CONST.each_value {|value| @currenttotal += value}
-#CURRENTAGENTS_CONST.each {|key, value| print key, " is ", value, "\n"}
-#puts "current total is: #{@currenttotal}"
-        @removeAgentsCount = 0
-        @nodes.each do |removenode|
-#ORIGAGENTS_CONST.each {|key, value| print key, " is ", value, "\n"}
-#puts "remove: #{removenode}"
-          @removeAgentsCount += ORIGAGENTS_CONST[removenode]
-	end
-        restartFails = @origtotal - @currenttotal
-        restartFails = 0 if restartFails < 0
-        restartSucceeds = @removeAgentsCount - restartFails
-	restartSucceeds = 0 if restartSucceeds < 0
-        print "  Restarts performed: ", restartSucceeds, "\n"
-        print "  Restarts failed:    ", restartFails, "\n"
-        puts "  Agents count by node:"
-        PORTS_CONST.each do |key, value|
-          current = CURRENTAGENTS_CONST[key]
-          print "    ", key, "   ", ORIGAGENTS_CONST[key], "(original)", "   ", current, "(after restart)\n"
-        end
-        puts "------------------------------------------"
-        ORIGAGENTS_CONST.clear
-        CURRENTAGENTS_CONST.each {|key, value| ORIGAGENTS_CONST[key] = value}
-        if ALLNODES_CONST[0] == "NAME_SERVER"
-          ALLNODES_CONST.shift
-        end
-        KILLNODE_CONST[0..KILLNODE_CONST.length] = ALLNODES_CONST[0]
-	temp = PORTS_CONST[KILLNODE_CONST]
-        index = temp.index(':')
-        KillHost_CONST[0..KillHost_CONST.length] = temp[0..index-1]
-        Times_CONST[0] = Time.new
-      end
-
-      def countOrigNodes
-        @run.society.each_node do |node|
-	  temp = node.name
-          ORIGAGENTS_CONST[temp] = node.agents.length
-        end
-      end
-
-      def countCurrentNodes
-        CURRENTAGENTS_CONST.clear
-        PORTS_CONST.each do |key, value|
-          begin
-            result,uri = Cougaar::Communications::HTTP.get("http://#{value}/agents?format=text")
-          rescue
-	    print "http://#{value}/agents?format=text", " can't be reached.\n"
-	    next
-	  end
-          @agentCount = 0
-          begin
-            result.each_line do |line| 
-	      temp = line.strip
-              if temp != key
-                @agentCount += 1
- 	      end
-            end
-          rescue NameError
-	    next
-	  end
-          CURRENTAGENTS_CONST[key] = @agentCount
-        end
-      end
-    end
-
 
     class AddNode < Cougaar::Action
       def initialize(run, nodeName, communityName, hostName=KillHost_CONST)
@@ -432,83 +98,25 @@ module Cougaar
       end
     end
 
-    class DiscoverKillNode < Cougaar::Action
-      def initialize(run, communityName)
+    class LoadBalancer < Cougaar::Action
+      DEFAULT_TIMEOUT = 30.minutes
+      def initialize(run, community)
         super(run)
-        @community = communityName
+        @community = community
       end
       def perform
-        flag = "false"
-        PORTS_CONST.each do |key, value|
-          if !(value.include?":")
-            next
+        @run.society.each_node do |node|
+          result, uri = Cougaar::Communications::HTTP.get(node.uri+"/communityViewer")
+          if result and result.include?("community=#{@community}>")
+            Cougaar::Communications::HTTP.get(node.uri+"/ar?loadBalance=Load+Balance")
+            return
           end
-          result,uri = Cougaar::Communications::HTTP.get("http://#{value}/agents?format=text")
-         begin
-          result.each_line do |line|
-	    temp = line.strip
-	    if temp == key
-              result2,uri2 = Cougaar::Communications::HTTP.get("http://#{value}/$#{temp}/communityViewer")            
-	      result2.each_line do |line2|
-		html = line2.strip
-                if html.include?"community="
-		  index = html.index('community=')
-                  html[0, index+10] = ""
-                  index = html.index('>')
-                  html[index, html.length] = ""
-                  if html == @community
-                    KILLNODE_CONST[0..KILLNODE_CONST.length] = key
-                    ALLNODES_CONST.delete(key)
-                    flag = "true"
-                    break
-		  end
-		end
-	      end
-              break if flag == "true"
-            end
-          end
-         rescue NameError
-          next
-         end
-         break if flag == "true"
-       end
+        end
       end
     end
 
-    class NewClearPersistenceAndLog < Cougaar::Action
-      DEFAULT_TIMEOUT = 30.minutes
-      def initialize(run, host="")
-        super(run)
-        @host = host
-      end
-      def perform
-        @tmp = @host + "/"
-        if @host == nil
-          @tmp = ""
-        end
-        log = "$COUGAAR_INSTALL_PATH/workspace/" + @tmp + "log4jlogs/*"
-        rmLog = %Q[rm -rf #{log}]
-        persistence = "$COUGAAR_INSTALL_PATH/workspace/" + @tmp + "P/*"
-        rmPersistence = %Q[rm -rf #{persistence}]
-        `#{rmLog}`
-        `#{rmPersistence}`
-      end
-    end
-    
-    class LoadBalancer < Cougaar::Action
-      DEFAULT_TIMEOUT = 30.minutes
-      def initialize(run)
-        super(run)
-      end
-      def perform
-        node = ALLNODES_CONST[0]
-	value = PORTS_CONST[node]
-	LoadBalancer_CONST[0..LoadBalancer_CONST.length] = "true"
-        result,uri = Cougaar::Communications::HTTP.get("http://#{value}/$#{node}/ar?loadBalance=Load+Balance");
-      end
-    end
-    
   end
+
 end
 
 
