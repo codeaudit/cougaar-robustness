2fa2b10e522a73d45c5d2e5ddf5cfe0325fcc9ea
sford <sford>
2003-07-01 23:43:03 +0000

    Add ACME run scripts.

--- msglog/csmart/config/scripts/robustness/uc2/RunUC1.rb
+++ msglog/csmart/config/scripts/robustness/uc2/RunUC1.rb
@@ -16,10 +16,11 @@ HOSTS_FILE = Ultralog::OperatorUtils::HostManager.new.get_hosts_file
 Cougaar::ExperimentMonitor.enable_stdout
 Cougaar::ExperimentMonitor.enable_logging
 
-Cougaar.new_experiment("UC2-Steve").run(1) {
+Cougaar.new_experiment("UC1-Steve").run(1) {
 
   do_action "LoadSocietyFromScript", "#{CIP}/configs/ul/FULL-1AD-TRANS-1359.rb"
   do_action "LayoutSociety", "#{CIP}/operator/1ad-layout-10_4_1.xml", HOSTS_FILE
+
   do_action "TransformSociety", false, 
     "#{RULES}/isat",
     "#{RULES}/logistics",
@@ -44,55 +45,13 @@ Cougaar.new_experiment("UC2-Steve").run(1) {
   do_action "Sleep", 30.seconds
   do_action "PublishNextStage"
 
-  # AR-UC1 (Sledgehammer)
-  #do_action "Sleep", 8.minutes
+  do_action "Sleep", 8.minutes
   #do_action "KillNodes", "REAR-D"
-  #do_action "KillNodes", "FWD-B"
-
-  # AR-UC2 (Rolling Partition)
-  do_action "Sleep", 1.minutes
-  #do_action "GenericAction" do |run|
-  #  run.comms.on_cougaar_event do |event|
-  #    puts event.data
-  #  end
-  #end
-  do_action "MonitorProtocolSelection", "FWD-B", "org.cougaar.core.mts.email.OutgoingEmailLinkProtocol"
-  do_action "MonitorProtocolSelection", "FWD-C", "org.cougaar.core.mts.email.OutgoingEmailLinkProtocol"
-  do_action "DisableNetworkInterfaces", "FWD-B"
-  do_action "GenericAction" do |run|
-    sleep 20.seconds
-  end
-  do_action "DisableNetworkInterfaces", "FWD-C"
-  do_action "EnableNetworkInterfaces", "FWD-B"
-  do_action "GenericAction" do |run|
-    sleep 20.seconds
-  end
-  do_action "DisableNetworkInterfaces", "FWD-B"
-  do_action "EnableNetworkInterfaces", "FWD-C"
-  do_action "GenericAction" do |run|
-    sleep 20.seconds
-  end
-  do_action "DisableNetworkInterfaces", "FWD-C"
-  do_action "EnableNetworkInterfaces", "FWD-B"
-  do_action "GenericAction" do |run|
-    sleep 20.seconds
-  end
-  do_action "DisableNetworkInterfaces", "FWD-B"
-  do_action "EnableNetworkInterfaces", "FWD-C"
-  do_action "GenericAction" do |run|
-    sleep 20.seconds
-  end
-  do_action "DisableNetworkInterfaces", "FWD-C"
-  do_action "EnableNetworkInterfaces", "FWD-B"
-  do_action "GenericAction" do |run|
-    sleep 20.seconds
-  end
-  do_action "EnableNetworkInterfaces", "FWD-C"
+  do_action "KillNodes", "FWD-B"
 
   wait_for  "PlanningComplete"  do
     wait_for  "Command", "shutdown"
     do_action "SaveSocietyCompletion", "completion_#{experiment.name}.xml"
-    do_action "EnableNetworkInterfaces", "FWD-B", "FWD-C"
     do_action "StopSociety"
     do_action "ArchiveLogs"
     do_action "StopCommunications"
@@ -103,7 +62,6 @@ Cougaar.new_experiment("UC2-Steve").run(1) {
   do_action "SaveSocietyCompletion", "completion_#{experiment.name}.xml"
 #stop to "P" in case you want to rehydrate"
   #do_action "Sleep", 10.minutes
-  do_action "EnableNetworkInterfaces", "FWD-B", "FWD-C"
   do_action "StopSociety"
   do_action "ArchiveLogs"
   do_action "StopCommunications"
