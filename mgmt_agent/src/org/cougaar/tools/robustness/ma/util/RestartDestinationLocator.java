8dd34e52af6424a40dc0316dd4fcaab8e601125c
rsnyder <rsnyder>
2003-07-16 16:41:25 +0000

    Inital update to support restart retry at alternate location when a restart failure is encountered.

--- mgmt_agent/src/org/cougaar/tools/robustness/ma/util/RestartDestinationLocator.java
+++ mgmt_agent/src/org/cougaar/tools/robustness/ma/util/RestartDestinationLocator.java
@@ -7,13 +7,14 @@ import org.cougaar.tools.robustness.ma.CommunityStatusChangeEvent;
 import org.cougaar.tools.robustness.ma.CommunityStatusModel;
 import org.cougaar.tools.robustness.ma.controllers.DefaultRobustnessController;
 
-import java.util.Hashtable;
+import java.util.*;
 
 
 public class RestartDestinationLocator {
   static CommunityStatusModel model;
   static LoggingService logger;
   static Hashtable restartAgents = new Hashtable();
+  static Hashtable selectedNodes = new Hashtable();
 
 
   public RestartDestinationLocator() {
@@ -27,24 +28,64 @@ public class RestartDestinationLocator {
     logger = ls;
   }
 
-  public static String getRestartLocation() {
+  public static String getRestartLocation(String agent) {
+    if(selectedNodes.containsKey(agent)) {
+      LinkedList nodes = (LinkedList)selectedNodes.get(agent);
+      if(nodes.size() > 0) {
+        return (String) nodes.removeFirst();
+      }
+      else {
+        selectedNodes.remove(agent);
+        return null;
+      }
+    }
+
     String candidateNodes[] =
         model.listEntries(model.NODE, DefaultRobustnessController.ACTIVE);
     int numAgents = 0;
     String selectedNode = null;
+    Hashtable temp = new Hashtable();
     for (int i = 0; i < candidateNodes.length; i++) {
       int agentsOnNode = model.agentsOnNode(candidateNodes[i]).length;
-      logger.debug("agents on node: " + candidateNodes[i] + "  " + agentsOnNode);
+      if(logger.isDebugEnabled())
+        logger.debug("agents on node: " + candidateNodes[i] + "  " + agentsOnNode);
       if(restartAgents.containsKey(candidateNodes[i])){
         int restarts = Integer.parseInt((String)(restartAgents.get(candidateNodes[i])));
         agentsOnNode += restarts;
       }
+      temp.put(candidateNodes[i], Integer.toString(agentsOnNode));
       if (selectedNode == null || agentsOnNode < numAgents) {
         selectedNode = candidateNodes[i];
         numAgents = agentsOnNode;
       }
     }
+    temp.remove(selectedNode); //this one is already used this time, don't count it.
+
+    LinkedList list = new LinkedList();
+    for(int i=0; i<temp.size(); i++) {
+      String next = getNextNode(temp);
+      list.addLast(next);
+      temp.remove(next);
+    }
+    if(logger.isDebugEnabled())
+      logger.debug("other avaliable restart nodes: " + list);
+    selectedNodes.put(agent, list);
+
     return selectedNode;
+  }
+
+  private static String getNextNode(Hashtable nodes) {
+    String next = "";
+    int number = 0;
+    for(Iterator it = nodes.keySet().iterator(); it.hasNext();) {
+      String key = (String)it.next();
+      int agents = Integer.parseInt((String)nodes.get(key));
+      if(number == 0 || agents < number) {
+        number = agents;
+        next = key;
+      }
+    }
+    return next;
   }
 
   public static void restartOneAgent(String nodeName) {
@@ -60,6 +101,11 @@ public class RestartDestinationLocator {
 
   public static void clearRestarts() {
     restartAgents.clear();
+  }
+
+  public static void restartSuccess(String agentName) {
+    if(selectedNodes.containsKey(agentName))
+      selectedNodes.remove(agentName);
   }
 
 }
