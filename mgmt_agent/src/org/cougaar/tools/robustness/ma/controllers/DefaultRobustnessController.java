b0fa680b7bf5cb4e04131bde386d9cecdb298236
rsnyder <rsnyder>
2003-09-02 13:12:26 +0000

    Added helper methods for evaluating Assets.

--- mgmt_agent/src/org/cougaar/tools/robustness/ma/controllers/DefaultRobustnessController.java
+++ mgmt_agent/src/org/cougaar/tools/robustness/ma/controllers/DefaultRobustnessController.java
@@ -20,6 +20,7 @@ package org.cougaar.tools.robustness.ma.controllers;
 import org.cougaar.tools.robustness.ma.CommunityStatusModel;
 import org.cougaar.tools.robustness.ma.controllers.*;
 import org.cougaar.tools.robustness.ma.HostLossThreatAlertHandler;
+import org.cougaar.tools.robustness.ma.SecurityAlertHandler;
 import org.cougaar.tools.robustness.ma.util.DeconflictHelper;
 import org.cougaar.tools.robustness.ma.util.DeconflictListener;
 import org.cougaar.tools.robustness.ma.util.HeartbeatListener;
@@ -407,6 +408,7 @@ public class DefaultRobustnessController extends RobustnessControllerBase {
     RestartDestinationLocator.setCommunityStatusModel(csm);
     RestartDestinationLocator.setLoggingService(logger);
     new HostLossThreatAlertHandler(getBindingSite(), agentId, this, csm);
+    new SecurityAlertHandler(getBindingSite(), agentId, this, csm);
   }
 
   boolean communityReady = false;
