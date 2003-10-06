0f3d66f2ad4d2e9d07c0c1c2d4f7fa34f138eaeb
rsnyder <rsnyder>
2003-10-06 23:34:39 +0000

    Updated ruby scripts.

---
+++ mgmt_agent/csmart/config/scripts/robustness/uc1/uc1-disconnect-moas.rb
@@ -0,0 +1,102 @@
+CIP = ENV['CIP']
+RULES = File.join(CIP, 'csmart','config','rules')
+
+$:.unshift File.join(CIP, 'csmart', 'acme_scripting', 'src', 'lib')
+$:.unshift File.join(CIP, 'csmart', 'acme_service', 'src', 'redist')
+$:.unshift File.join(CIP, 'csmart', 'config', 'lib')
+
+require 'cougaar/scripting'
+require 'ultralog/scripting'
+require 'robustness/uc2/msglog'
+require 'robustness/uc1/aruc1_actions_and_states'
+require 'robustness/uc1/deconfliction'
+require 'robustness/uc7/disconnection'
+
+HOSTS_FILE = Ultralog::OperatorUtils::HostManager.new.get_hosts_file
+
+Cougaar::ExperimentMonitor.enable_stdout
+Cougaar::ExperimentMonitor.enable_logging
+
+Cougaar.new_experiment("ARUC1_Planned_Disconnect").run(1) {
+
+  do_action "LoadSocietyFromScript", "#{CIP}/csmart/config/societies/ua/full-160a237v.plugins.rb"
+  do_action "LayoutSociety", "#{CIP}/operator/Full-UA-21H51N537A-NoG-layout.xml", HOSTS_FILE
+
+  do_action "TransformSociety", false,
+    "#{RULES}/isat",
+    "#{RULES}/logistics",
+    "#{RULES}/robustness",
+    "#{RULES}/robustness/uc1",
+    "#{RULES}/metrics/basic",
+    "#{RULES}/metrics/sensors",
+    "#{RULES}/robustness/uc9/deconfliction.rule",
+    "#{RULES}/robustness/uc7/disconnection.rule"
+
+  do_action "TransformSociety", false, "#{RULES}/robustness/communities"
+
+  do_action "SaveCurrentSociety", "mySociety.xml"
+  do_action "SaveCurrentCommunities", "myCommunities.xml"
+
+  do_action "StartJabberCommunications"
+  do_action "CleanupSociety"
+  do_action "VerifyHosts"
+
+  do_action "DeployCommunitiesFile"
+
+  do_action "ConnectOperatorService"
+  do_action "ClearPersistenceAndLogs"
+  do_action "InstallCompletionMonitor"
+
+  do_action "StartSociety"
+
+  ## Print events from Robustness Controller
+  do_action "GenericAction" do |run|
+    run.comms.on_cougaar_event do |event|
+      if event.component.include?("RobustnessController")
+        puts event
+      end
+    end
+  end
+
+  # After CommunityReady event is received wait for persistence
+  #wait_for "CommunitiesReady", ["CONUS-REAR-COMM"]
+
+  wait_for  "GLSConnection", true
+  wait_for  "NextOPlanStage"
+  do_action "Sleep", 30.seconds
+  do_action "PublishNextStage"
+
+# Deconfliction start
+  do_action "Sleep", 30.seconds
+  do_action "MonitorProtocolSelection"
+  do_action "MonitorMsglogEnabling"
+#  do_action "MonitorPlannedDisconnectExpired"
+  do_action "MonitorDisconnectionEvents"
+  do_action "StartPlannedDisconnect", "REAR-A-NODE", 600
+  wait_for  "PlannedDisconnectStarted", "REAR-A-NODE"
+  do_action "DisableNetworkInterfaces", "REAR-A-NODE"
+  do_action "Sleep", 10.minutes
+  do_action "EnableNetworkInterfaces", "REAR-A-NODE"
+  do_action "EndPlannedDisconnect", "REAR-A-NODE"
+#  wait_for  "ReconnectConfirmed", "REAR-A-NODE"
+# Deconfliction end
+
+  wait_for  "SocietyQuiesced"  do
+    wait_for  "Command", "shutdown"
+    do_action "SaveSocietyCompletion", "completion_#{experiment.name}.xml"
+    #include "inventory.inc", "RunSoc"
+    do_action "StopSociety"
+    do_action "ArchiveLogs"
+    do_action "StopCommunications"
+  end
+
+  wait_for "Command", "shutdown"
+  do_action "Sleep", 30.seconds
+  do_action "SaveSocietyCompletion", "completion_#{experiment.name}.xml"
+  #include "inventory.inc", "RunSoc"
+  do_action "Sleep", 30.seconds
+  do_action "StopSociety"
+  do_action "ArchiveLogs"
+  do_action "StopCommunications"
+
+}
