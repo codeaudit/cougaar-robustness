913ba537c542de659af5e248ba14cfbcbd3f7352
wells <wells>
2004-05-20 17:54:04 +0000

    added missing files

---
+++ Coordinator/src/org/cougaar/coordinator/costBenefit/BadWeightsException.java
@@ -0,0 +1,20 @@
+/*
+ * BadWeightsException.java
+ *
+ * Created on May 13, 2004, 12:52 PM
+ */
+
+package org.cougaar.coordinator.costBenefit;
+
+/**
+ *
+ * @author  David Wells - OBJS
+ * @version 
+ */
+public class BadWeightsException extends Exception {
+
+    /** Creates new BadWeightsException */
+    public BadWeightsException() {
+    }
+
+}
