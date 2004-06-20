6f81b64b5103e190c2c87e6107edf4622c1627e7
pazandak <pazandak>
2004-06-20 01:01:22 +0000

    Added facade method to retrieve an action description using an action value from an action record

--- Coordinator/src/org/cougaar/coordinator/test/coordination/TestActionTechSpec.java
+++ Coordinator/src/org/cougaar/coordinator/test/coordination/TestActionTechSpec.java
@@ -64,6 +64,16 @@ public class TestActionTechSpec implements ActionTechSpecInterface {
         return null;
     }
     
+    /**
+     * @return an action description for the specified actionValue taken
+     * from the actionRecord.getValue(), which was generated initially from getValueFromXML(). 
+     * The tech spec does not know what this conversion is since it didn't have a handle to an Action
+     * at start up.
+     */
+    public ActionDescription getActionDescriptionForValue(Action a, Object actionValue) { 
+        return null;
+    }
+    
     /** @return the ActionDescriptions for this Actuator
      */
     public void addAction(ActionDescription ad) {
