6eef787fe7c88124a50f8d586843ee01d1c67a6b
sford <sford>
2004-08-11 20:15:49 +0000

    Add more agents and messaging.

--- msglog/test/SampleDefense/runNodes.bat
+++ msglog/test/SampleDefense/runNodes.bat
@@ -1,4 +1,6 @@
 REM start %COUGAAR_INSTALL_PATH%\bin\Node  SomeNode
 REM sleep 10
-start %COUGAAR_INSTALL_PATH%\bin\Node  T1
 start %COUGAAR_INSTALL_PATH%\bin\Node  ManagementAgentNode
+sleep 10
+start %COUGAAR_INSTALL_PATH%\bin\Node  NodeA
+start %COUGAAR_INSTALL_PATH%\bin\Node  NodeB
