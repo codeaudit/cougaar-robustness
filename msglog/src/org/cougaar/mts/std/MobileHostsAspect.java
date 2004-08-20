c9a4e6a95280c76e4c392cfb18e1418afa7ae317
sford <sford>
2004-08-20 16:40:01 +0000

    Simplified.

--- msglog/src/org/cougaar/mts/std/MobileHostsAspect.java
+++ msglog/src/org/cougaar/mts/std/MobileHostsAspect.java
@@ -97,16 +97,11 @@ public class MobileHostsAspect extends StandardAspect
 	schedulable = threadSvc.getThread(this, this, "MobileHostsAspect");
 	schedulable.schedule(0, period);  // run now and every period milliseconds thereafter
 	sb.releaseService(this, ThreadService.class, threadSvc);
-
-	Security.setProperty("networkaddress.cache.ttl", "10");
-
     }
 
     public Object getDelegate (Object delegate, Class type) {
 	if (type == SendLink.class)
 	    return new SendLinkDelegate((SendLink)delegate);
-//	if (type == DestinationLink.class)
-//	    return new DestinationLinkDelegate((DestinationLink)delegate);
 	return null;
     }
 
@@ -136,127 +131,44 @@ public class MobileHostsAspect extends StandardAspect
 	}
     }
 
-/*    
-    private class DestinationLinkDelegate extends DestinationLinkDelegateImplBase 
-    {
-	DestinationLink link;
-
-	private DestinationLinkDelegate (DestinationLink link) {
-	    super(link);
-	    this.link = link;
-	}
-	
-	public MessageAttributes forwardMessage (AttributedMessage msg) 
-	    throws UnregisteredNameException, NameLookupException, 
-		   CommFailureException, MisdeliveredMessageException
-	{
-	    return super.forwardMessage(msg);
-	}
-	
-    }
-*/
-
     public void run() {
 	try {
-	    Enumeration nifs = NetworkInterface.getNetworkInterfaces();
+	    InetAddress inetAddr = InetAddress.getLocalHost();
 	    if (log.isDetailEnabled())
-		log.detail("NetworkInterface.getNetworkInterfaces()="+nifs);
-	    while (nifs.hasMoreElements()) {
-		NetworkInterface nif = (NetworkInterface)nifs.nextElement();
-		if (log.isDetailEnabled())
-		    log.detail("NetworkInterface="+nif);
-                if (nif.getName().startsWith("eth0")) {
-		    Enumeration addrs = nif.getInetAddresses();
-		    int addrCount = 0;
-		    while (addrs.hasMoreElements()) {
-			InetAddress inAddr = (InetAddress)addrs.nextElement();
-			if (log.isDetailEnabled())
-			    log.detail("InetAddress="+inAddr);
-			if (inAddr.isLoopbackAddress())
-			    continue;
-			String addr = inAddr.getHostAddress();
-			if (log.isDetailEnabled())
-			    log.detail("addr="+addr);
-			addrCount++;
-			if (addrCount > 1) {
-			    if (log.isErrorEnabled())
-				log.error("More than one non-Loopback InetAddress for NetworkInterface="+nif);
-			    continue;
-			}			    
-			if (myAddr == null) {
-			    myAddr = addr;
-			    myHost = inAddr.getHostName();
-			    if (log.isInfoEnabled()) {
-				log.info("myAddr initialized to "+myAddr);
-				log.info("myHost initialized to "+myHost);
-			    }
-			} else if (!addr.equals(myAddr)) {
-			    if (log.isInfoEnabled())
-				log.info("myAddr has changed from "+myAddr+" to "+addr);
-			    synchronized (clients) {
-				Iterator iter = clients.iterator();
-				while (iter.hasNext()) {
-				    MessageTransportClient client = (MessageTransportClient)iter.next();
-				    if (log.isInfoEnabled())
-					log.info("run: unregister "+client);
-				    registry.unregisterClient(client);
-				    /*
-				    String newHostName = addr.getHostName();
-				    if (log.isInfoEnabled())
-					log.info("run: getHostName() of new addr="+addr+" returned "+newHostName);
-				    if (newHostName!=null && newHostName.equals(myHost)) {  // if new lookup succeeds
-					myAddr = addr;
-					registry.registerClient(client);
-				    }
-				    */
-				    /*
-				    InetAddress[] addresses = null;
-				    try {
-					addresses = InetAddress.getAllByName(myHost);
-					if (addresses != null) {
-					    for (int i=0 ; i<addresses.length ; i++) {
-						InetAddress thisInAddr = addresses[i];
-						if (log.isDetailEnabled())
-						    log.detail("run: getAllByName("+myHost+") returned "+thisInAddr);
-						String thisAddr = thisInAddr.getHostAddress();
-						if (thisAddr.equals(addr)) {
-						    if (log.isInfoEnabled())
-							log.info("run: "+myHost+" rebound to new address "+addr+".  Registering client "+client);
-						    myAddr = addr;
-						    registry.registerClient(client);}
-					    }					
-					}
-				    } catch (java.net.UnknownHostException e) {
-					log.error("Host "+myHost+" no longer known.", e);
-				    }
-				    */
-				    InetAddress newInetAddr = null;
-				    try {
-					newInetAddr = InetAddress.getLocalHost();
-					if (newInetAddr != null) {
-					    if (log.isDetailEnabled())
-						log.detail("run: getLocalHost() returned "+newInetAddr);
-					    String newAddr = newInetAddr.getHostAddress();
-					    String newHost = newInetAddr.getHostName();
-					    if (newAddr.equals(addr) && newHost.equals(myHost)) {
-						if (log.isInfoEnabled())
-						    log.info("run: "+myHost+" rebound to new address "+addr+".  Registering client "+client);
-						myAddr = addr;
-						registry.registerClient(client);
-					    }
-					}					
-				    } catch (java.net.UnknownHostException e) {
-					log.error("getLocalHost returned an unknown host.", e);
-				    }
-				}
-			    }
-			}
-		    }
-		}
+		log.detail("run: InetAddress="+inetAddr);
+	    String addr = inetAddr.getHostAddress();
+	    String host = inetAddr.getHostName();
+	    if (log.isDetailEnabled()) {
+		log.detail("run: addr="+addr);
+		log.detail("run: host="+host);
 	    }
-	} catch (java.net.SocketException e) {
-	    log.error("SocketException calling NetworkInterface.getNetworkInterfaces()", e);
+	    if (myAddr == null && myHost == null) {
+		myAddr = addr;
+		myHost = host;
+		if (log.isInfoEnabled()) {
+		    log.info("run: myAddr initialized to "+myAddr);
+		    log.info("run: myHost initialized to "+myHost);
+		}
+	    } else if (!addr.equals(myAddr) && host.equals(myHost)) {
+		if (log.isInfoEnabled())
+		    log.info("run: "+myHost+"'s address changed from "+myAddr+" to "+addr+".  Re-registering clients.");
+		myAddr = addr;
+		synchronized (clients) {
+		    Iterator iter = clients.iterator();
+		    while (iter.hasNext()) {
+			MessageTransportClient client = (MessageTransportClient)iter.next();
+			if (log.isInfoEnabled())
+			    log.info("run: Unregistering client "+client);
+			registry.unregisterClient(client);
+			if (log.isInfoEnabled())
+			    log.info("run: Registering client "+client);		
+			registry.registerClient(client);
+		    }
+		}					
+	    }
+	} catch (java.net.UnknownHostException e) {
+	    log.error("run: getLocalHost returned an unknown host.", e);
 	}
     }
-    
+
 }
