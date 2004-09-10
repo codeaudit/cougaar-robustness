2b9d0adadf76af0b015859a10ef36f5bc66f3c61
wells <wells>
2004-09-10 01:47:45 +0000

    Fixed an NPE

--- disconnection/src/org/cougaar/tools/robustness/disconnection/InternalConditionsAndOpModes/ReconnectTimeCondition.java
+++ disconnection/src/org/cougaar/tools/robustness/disconnection/InternalConditionsAndOpModes/ReconnectTimeCondition.java
@@ -93,7 +93,9 @@ public class ReconnectTimeCondition extends DefenseTimeCondition
     
      public int updateContent(Object content, Relay.Token token) {
         ReconnectTimeCondition newRTC = (ReconnectTimeCondition) content;
-        if (getValue().compareTo(newRTC.getValue()) != 0 ||
+        if (getValue()==null && newRTC.getValue()==null) return Relay.NO_CHANGE;
+        if (getValue()==null ||
+            getValue().compareTo(newRTC.getValue()) != 0 ||
             !newRTC.getAgents().equals(agents)) {
           setValue(newRTC.getValue());
           agents = newRTC.getAgents();
