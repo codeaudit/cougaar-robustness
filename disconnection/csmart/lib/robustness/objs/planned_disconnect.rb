2cd24fb4553ece6a5c48cd44f0a014d95f7df5f0
sford <sford>
2004-09-13 21:08:18 +0000

    Remove 2 minute delay before starting experiment.

--- disconnection/csmart/lib/robustness/objs/planned_disconnect.rb
+++ disconnection/csmart/lib/robustness/objs/planned_disconnect.rb
@@ -14,7 +14,6 @@ insert_after parameters[:location] do
   planned = eval(parameters[:planned_disconnect].to_s)
   actual = eval(parameters[:actual_disconnect].to_s)
   verb = parameters[:verbose]
-  do_action "Sleep", 2.minutes
   do_action "PlannedDisconnect", parameters[:nodes], planned, actual, verb
 end
 
