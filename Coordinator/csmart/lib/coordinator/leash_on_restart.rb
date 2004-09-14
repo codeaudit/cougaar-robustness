6925892ef4403c1346b58cb43990b2f05a754142
sford <sford>
2004-09-14 14:35:11 +0000

    Make this script Unleash Defenses after :starting_stage.

--- Coordinator/csmart/lib/coordinator/leash_on_restart.rb
+++ Coordinator/csmart/lib/coordinator/leash_on_restart.rb
@@ -1,7 +1,7 @@
 =begin script
 
 include_path: leash_on_restart.rb
-description: Leash All Defenses on a Full Society Restore
+description: Leash All Defenses on a Full Society Restore and Unleash them at starting_stage
 
 =end
 
@@ -12,4 +12,10 @@ require 'coordinator/leashing'
 
 insert_after :snapshot_restored do
   do_action "LeashOnRestart"
+end
+
+insert_after :starting_stage do
+  do_action "MonitorUnleash", 1
+  do_action "Unleash", 1
+  wait_for "Unleashed", 1
 end
