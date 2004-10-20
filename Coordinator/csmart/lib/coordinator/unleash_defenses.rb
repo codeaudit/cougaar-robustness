d8523bfa316f1bb840df340e3e351f9fa681b4e3
sford <sford>
2004-10-20 17:17:55 +0000

    Add a start_delay parameter

--- Coordinator/csmart/lib/coordinator/unleash_defenses.rb
+++ Coordinator/csmart/lib/coordinator/unleash_defenses.rb
@@ -11,6 +11,11 @@ $:.unshift File.join(CIP, 'csmart', 'config', 'lib')
 require 'coordinator/leashing'
 
 insert_after parameters[:location] do
+ if( parameters[:start_delay] != nil && parameters[:start_delay] > 0)
+    do_action "SleepFrom", parameters[:location], parameters[:start_delay]
+  else
+    do_action "SleepFrom", parameters[:location], 60
+  end
   do_action "Unleash", parameters[:verbose]
   do_action "UnleashOnSubsequentRestarts", parameters[:verbose]
 end
