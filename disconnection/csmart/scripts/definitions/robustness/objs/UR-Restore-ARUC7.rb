c20f77b1da9534fb4484561e582f1b11a6ba54c7
sford <sford>
2004-10-21 21:53:51 +0000

    Update run scripts.

--- disconnection/csmart/scripts/definitions/robustness/objs/UR-Restore-ARUC7.rb
+++ disconnection/csmart/scripts/definitions/robustness/objs/UR-Restore-ARUC7.rb
@@ -14,18 +14,25 @@ parameters:
 include_scripts:
   - script: $CIP/csmart/lib/isat/clearLogs.rb
   - script: $CIP/csmart/lib/isat/initialize_network.rb
-
+  - script: $CIP/csmart/lib/coordinator/leash_on_restart.rb 
   - script: $CIP/csmart/lib/robustness/objs/planned_disconnect.rb
     parameters:
-      - location: starting_stage
-      - start_delay: 60
-      - wait_location: after_stage
+      - location: during_stage_2
+      - wait_location: after_stage_2
       - nodes: ["UA-FSB-A-NODE", "UA-FSB-C-NODE"]
       - planned_disconnect: 12.minutes
       - actual_disconnect: 8.minutes
       - timeout: 30.minutes
       - verbose: 2
-  - script: $CIP/csmart/lib/coordinator/leash_on_restart.rb 
+  - script: $CIP/csmart/lib/coordinator/unleash_defenses.rb 
+    parameters:
+      - location: during_stage_2
+      - verbose: 1
+  - script: $CIP/csmart/lib/coordinator/nodes_persisted_find_providers.rb 
+    parameters:
+      - location: during_stage_2
+      - start_delay: 60
+      - nodes: ["UA-FSB-A-NODE", "UA-FSB-C-NODE"]
 
 =end
 
