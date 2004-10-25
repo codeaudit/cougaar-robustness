b7048a6438e6950252b6b639a71d81573423b3b9
wells <wells>
2004-10-25 22:18:02 +0000

    Script to change MAU weights in AgentCompromise experiment duing restore phase.

--- Coordinator/csmart/scripts/definitions/robustness/objs/ACUC2A-UA-Restore-Stage5-AgentCompromiseModerate.rb
+++ Coordinator/csmart/scripts/definitions/robustness/objs/ACUC2A-UA-Restore-Stage5-AgentCompromiseModerate.rb
@@ -2,13 +2,13 @@
 
 name: ACUC-2-AgentCompromiseModerate
 description: MAU-sensitive Coordiantor response to AgentCompromise - Stop After Stage1
-script: $CIP/csmart/scripts/definitions/RestoreTemplate.rb
+script: $CIP/csmart/scripts/definitions/UR-RestoreTemplate.rb
 parameters:
   - run_count: 1
   - snapshot_name: $CIP/SAVE-PreStage5-ARUC7.tgz
   - archive_dir: $CIP/Logs
   - stages:
-    - 1
+    - 5
   
 include_scripts:
   - script: $CIP/csmart/lib/isat/clearPnLogs.rb
@@ -16,20 +16,11 @@ include_scripts:
   - script: $CIP/csmart/lib/coordinator/leash_on_restart.rb
   - script: $CIP/csmart/lib/isat/stop_society.rb
     parameters:
-      - stop_location: after_stage_6
-  - script: $CIP/csmart/lib/logistics/al_data_compromise.rb
-  - script: $CIP/csmart/lib/coordinator/mauHighCompleteness.rb
-    parameters:
-      - location: during_stage_6
-      - start_delay: 300
-      - wait_location: after_stage_6
-      - verbose: 2
+      - stop_location: after_stage_5
   - script: $CIP/csmart/lib/logistics/al_data_compromise.rb
   - script: $CIP/csmart/lib/coordinator/mauHighSecurity.rb
     parameters:
-      - location: during_stage_6
-      - start_delay: 60
-      - wait_location: after_stage_6
+      - location: before_stage_5
       - verbose: 2
 =end
 
