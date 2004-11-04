0bb0f578920cb396c9400ecad8d707f73f7f40e1
wells <wells>
2004-11-04 19:13:37 +0000

    ditto

--- Coordinator/src/org/cougaar/coordinator/costBenefit/VariantEvaluation.java
+++ Coordinator/src/org/cougaar/coordinator/costBenefit/VariantEvaluation.java
@@ -85,8 +85,8 @@ public class VariantEvaluation implements Comparable {
         else if (otherVE.failedP()) return -1;
         //else if (otherVE.getBenefitToCostRatio() < this.getBenefitToCostRatio()) return -1;  dlw - greedy selection based on the ratio doesn't work
         //else if (otherVE.getBenefitToCostRatio() == this.getBenefitToCostRatio()) return 0; 
-        else if (otherVE.getBenefit() < this.getBenefit()) return -1;
-        else if (otherVE.getBenefit() == this.getBenefit()) return 0; 
+        else if (otherVE.getPredictedBenefit() < this.getPredictedBenefit()) return -1;
+        else if (otherVE.getPredictedBenefitt() == this.getPredictedBenefit()) return 0; 
         else return 1;
     }
 
