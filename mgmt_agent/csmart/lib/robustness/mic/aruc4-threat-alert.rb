21cf039e5171040bd1cdb089e345a26b2119aebf
rsnyder <rsnyder>
2004-11-08 19:49:15 +0000

    Scripts for testing ARUC4.

---
+++ mgmt_agent/csmart/lib/robustness/mic/aruc4-threat-alert.rb
@@ -0,0 +1,25 @@
+=begin script
+
+include_path: aruc4-threat-alert.rb
+description: Generate Threat Alert
+
+=end
+
+CIP = ENV['CIP']
+
+$:.unshift File.join(CIP, 'csmart', 'config', 'lib')
+require 'robustness/uc4/aruc4_actions_and_states'
+
+insert_after :during_stage_1 do
+  wait_for "NodesPersistedFindProviders", "REAR-A-NODE, REAR-B-NODE"
+assets = {'node' => 'REAR-A-NODE, REAR-B-NODE'}
+do_action "InfoMessage", "########  PublishThreatAlert - MAXIMUM  #########"
+do_action "PublishThreatAlert",
+          "org.cougaar.tools.robustness.ma.HostLossThreatAlert",
+          "community",
+          "REAR-COMM",
+          "maximum",
+          10.minutes,
+          assets,
+          "HealthMonitor"
+end
---
+++ mgmt_agent/csmart/scripts/definitions/robustness/mic/ALAR-UC4-PreStage2.rb
@@ -0,0 +1,49 @@
+=begin experiment
+
+name: ALAR_UC4
+description: ALAR-UC4 pre-Stage2
+script: $CIP/csmart/scripts/definitions/BaselineTemplate-ExtOplan.rb
+parameters:
+  - run_count: 1
+  - society_file: $CIP/csmart/config/societies/ua/full-tc20-avn-162a208v.plugins.rb
+  - layout_file: $CIP/operator/layouts/UR-557-layout-1.xml
+  - archive_dir: $CIP/Logs
+
+  - rules:
+    - $CIP/csmart/config/rules/isat
+    - $CIP/csmart/config/rules/yp
+    - $CIP/csmart/config/rules/logistics
+    - $CIP/csmart/config/rules/assessment
+
+    - $CIP/csmart/config/rules/robustness/manager.rule
+    - $CIP/csmart/config/rules/coordinator
+    - $CIP/csmart/config/rules/robustness/uc1
+    - $CIP/csmart/config/rules/robustness/uc1/debug
+    - $CIP/csmart/config/rules/robustness/uc4
+    #- $CIP/csmart/config/rules/robustness/uc7
+    #- $CIP/csmart/config/rules/robustness/uc9
+    #- $CIP/csmart/config/rules/robustness/UC3
+    - $CIP/csmart/config/rules/metrics/basic
+    - $CIP/csmart/config/rules/metrics/sensors
+    - $CIP/csmart/config/rules/metrics/serialization/metrics-only-serialization.rule
+    - $CIP/csmart/config/rules/metrics/rss/tic
+
+  - community_rules:
+    - $CIP/csmart/config/rules/robustness/communities
+
+include_scripts:
+  - script: $CIP/csmart/lib/isat/clearPnLogs.rb
+  - script: $CIP/csmart/lib/isat/initialize_network.rb
+
+  # Robustness scripts
+  - script: $CIP/csmart/lib/robustness/mic/aruc4-threat-alert.rb
+  #- script: $CIP/csmart/lib/coordinator/unleash_defenses.rb
+
+  - script: $CIP/csmart/lib/isat/stop_society.rb
+    parameters:
+      - stop_location: before_stage_2
+
+=end
+
+require 'cougaar/scripting'
+Cougaar::ExperimentDefinition.register(__FILE__)
